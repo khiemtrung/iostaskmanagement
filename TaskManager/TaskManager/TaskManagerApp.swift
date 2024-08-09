5d1ecfd6f370578b6e16c593ee85b9d0cb036aa4
KhiemTrung <trungkhiem@gmail.com>
2024-08-10 01:03:42 +0700

    Init project

---
+++ TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Khiem Nguyen Trung on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Views/TaskListView.swift
@@ -0,0 +1,8 @@
+//
+//  TaskListView.swift
+//  TaskManager
+//
+//  Created by Khiem Nguyen Trung on 10/08/2024.
+//
+
+import Foundation
